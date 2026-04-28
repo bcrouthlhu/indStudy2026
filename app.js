476c8edcf8b2d26460871128ff7d3ce346212902
bcrouthlhu <bcrouthlhu@gmail.com>
2026-04-28 18:27:14 -0400

    graphs

--- app.js
+++ app.js
@@ -136,6 +136,7 @@ passport.use(new MicrosoftStrategy({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.static('public'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
